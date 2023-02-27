c90c2b85cdbfa6601aa24921ef2a624252204329
KirillKazak <kazzak.kirill1995@gmail.com>
2023-02-27 18:49:31 +0300

    Layout Profile screen

---
+++ app/src/main/java/com/kazak/kirill/shoperset/ui/profile/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.kazak.kirill.shoperset.ui.profile
+
+import androidx.fragment.app.Fragment
+import com.kazak.kirill.shoperset.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+}
---
+++ app/src/main/java/com/kazak/kirill/shoperset/util/customViews/CustomViewItemProfile.kt
@@ -0,0 +1,40 @@
+package com.kazak.kirill.shoperset.util.customViews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import com.kazak.kirill.shoperset.R
+import com.kazak.kirill.shoperset.databinding.CustomViewItemProfileBinding
+
+class CustomViewItemProfile(context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
+
+    private val vbItemProfile = CustomViewItemProfileBinding.inflate(
+        LayoutInflater.from(context), this, false
+    )
+
+    init {
+        addView(vbItemProfile.root)
+        val typedArray = context.theme.obtainStyledAttributes(
+            attrs, R.styleable.CustomViewItemProfile, 0, 0
+        )
+
+        vbItemProfile.apply {
+            val iconItem = typedArray.getDrawable(R.styleable.CustomViewItemProfile_icon_item)
+            val nameItem = typedArray.getString(R.styleable.CustomViewItemProfile_name_item)
+            val visibilityBalance = typedArray.getString(R.styleable.CustomViewItemProfile_visibility_balance)
+
+            ivItemImage.background = iconItem
+            tvItemName.text = nameItem
+
+            if (visibilityBalance == "visible") {
+                ivArrow.visibility = View.INVISIBLE
+                tvBalanceScore.visibility = View.VISIBLE
+            } else  {
+                ivArrow.visibility = View.VISIBLE
+                tvBalanceScore.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
